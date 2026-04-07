5bc2929c64b6a66311bdc21b5499e29152c9ae3e
Mariangel Campos <mariangelcampos0217@gmail.com>
2026-04-07 10:05:20 -0400

    feat: Se integro el patron Adapter en el flujo principal.

--- main/Main.java
+++ main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import factory.*;
+import adapter.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +9,11 @@ public class Main {
         // CONFIGURACIÓN
         Nave nave = NaveFactory.crearNave("exploracion", "Orion");
         nave.mostrarTipo();
+
+        // PREPARACION (ADAPTER)
+        SistemaViejo viejo = new SistemaViejo();
+        SistemaExterno sistema = new AdaptadorSistema(viejo);
+        sistema.activar();
+        
     }
 }
