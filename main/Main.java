e4a9bda56b892c0278123f468487d52c092a8dcb
Mariangel Campos <mariangelcampos0217@gmail.com>
2026-04-07 09:40:49 -0400

    feat: Integración del patrón Factory en el flujo principal.

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import factory.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto Stellaris iniciado...");
+
+        // CONFIGURACIÓN
+        Nave nave = NaveFactory.crearNave("exploracion", "Orion");
+        nave.mostrarTipo();
     }
 }
