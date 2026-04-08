b657cc724e6ebab6871b9378db1357ac2521cae1
Mariangel Campos <mariangelcampos0217@gmail.com>
2026-04-08 09:25:45 -0400

    feat: Se implemento el sistema de registro de eventos de misión al archivo principal.

--- main/Main.java
+++ main/Main.java
@@ -1,34 +1,41 @@
 package main;
 
 import factory.*;
-import observer.BaseEspacial;
-import observer.NaveObservable;
 import adapter.*;
 import state.*;
+import observer.*;
+import utils.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        RegistroMision registro = new RegistroMision();
+
         // CONFIGURACIÓN
         Nave nave = NaveFactory.crearNave("exploracion", "Orion");
         nave.mostrarTipo();
+        registro.agregarEvento("Nave creada");
 
-        // PREPARACION (ADAPTER)
+        // PREPARACIÓN
         SistemaViejo viejo = new SistemaViejo();
         SistemaExterno sistema = new AdaptadorSistema(viejo);
         sistema.activar();
+        registro.agregarEvento("Sistema adaptado");
 
-         // OBSERVER
+        // OBSERVER
         NaveObservable naveObs = new NaveObservable();
         BaseEspacial base = new BaseEspacial();
-
         naveObs.agregarObservador(base);
         naveObs.enviarMensaje("Misión iniciada");
+        registro.agregarEvento("Mensaje enviado");
 
         // SIMULACIÓN
         Mision mision = new Mision();
         mision.mostrarEstado();
-       
+
+        // REPORTE
+        registro.mostrarReporte();
+    
     }
 
 }
